94e1b4dfc1d943f2ef43bc59ec8d622431cc05a8
Jedde Bowman <jjbowman2@bitbucket.org>
2019-11-13 07:59:52 -0700

    Adding Cole's code for AssignmentApiController

--- api/src/main/java/io/swagger/api/AssignmentsApiController.java
+++ api/src/main/java/io/swagger/api/AssignmentsApiController.java
@@ -1,24 +1,22 @@
 package io.swagger.api;
 
-import io.swagger.model.Assignment;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.*;
+import io.swagger.annotations.ApiParam;
+import io.swagger.model.Assignment;
+import io.swagger.repository.AssignmentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.*;
-import javax.validation.Valid;
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-10-25T16:55:34.601Z")
 
@@ -30,6 +28,9 @@ public class AssignmentsApiController implements AssignmentsApi {
     private final ObjectMapper objectMapper;
 
     private final HttpServletRequest request;
+
+    @Autowired
+    private AssignmentRepository assignmentRepository;
 
     @org.springframework.beans.factory.annotation.Autowired
     public AssignmentsApiController(ObjectMapper objectMapper, HttpServletRequest request) {
@@ -49,64 +50,99 @@ public class AssignmentsApiController implements AssignmentsApi {
 
     public ResponseEntity<Void> completeAssignment(@ApiParam(value = "",required=true) @PathVariable("assignmentId") Long assignmentId) {
         String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+        if (accept != null && accept.contains("application/json")) {
+            Iterator<Assignment> accountIterator = assignmentRepository.findAll().iterator();
+            while(accountIterator.hasNext())
+            {
+                Assignment assignment = accountIterator.next();
+                if(assignment.getAssignmentId().equals(assignmentId)){
+                    assignment.setIsComplete(true);
+                    assignmentRepository.save(assignment);
+                    return new ResponseEntity<Void>(HttpStatus.OK);
+                }
+            }
+        }
+        return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
     }
 
     public ResponseEntity<Void> createAssignment(@ApiParam(value = "" ,required=true )  @Valid @RequestBody Assignment body) {
         String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+        if (accept != null && accept.contains("application/json")) {
+            if (assignmentRepository.exists(body.getAssignmentId())) {
+                return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+            }
+            else {
+                assignmentRepository.save(body);
+                return new ResponseEntity<Void>(HttpStatus.CREATED);
+            }
+        }
+        return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
     }
 
     public ResponseEntity<Void> deleteAssignment(@ApiParam(value = "",required=true) @PathVariable("assignmentId") Long assignmentId) {
         String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+        if (accept != null && accept.contains("application/json")) {
+            if (assignmentRepository.findOne(assignmentId) == null) {
+                return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+            }
+            else {
+                assignmentRepository.delete(assignmentId);
+                return new ResponseEntity<Void>(HttpStatus.CREATED);
+            }
+        }
+        return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
     }
 
     public ResponseEntity<List<Assignment>> getAllAssignments() {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<Assignment>>(objectMapper.readValue("[ {  \"assignedAsurite\" : \"smurra11\",  \"evalType\" : \"p2p\",  \"asurite\" : \"jjbowma2\",  \"semester\" : \"Fall 2019\",  \"assignmentId\" : 54,  \"isComplete\" : false}, {  \"assignedAsurite\" : \"smurra11\",  \"evalType\" : \"p2p\",  \"asurite\" : \"jjbowma2\",  \"semester\" : \"Fall 2019\",  \"assignmentId\" : 54,  \"isComplete\" : false} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Assignment>>(HttpStatus.INTERNAL_SERVER_ERROR);
+            List<Assignment> assignmentList = new ArrayList<Assignment>();
+            Iterator<Assignment> accountIterator = assignmentRepository.findAll().iterator();
+            while(accountIterator.hasNext())
+            {
+                Assignment assignment = accountIterator.next();
+                assignmentList.add(assignment);
             }
+            return new ResponseEntity<List<Assignment>>(assignmentList, HttpStatus.OK);
         }
-
-        return new ResponseEntity<List<Assignment>>(HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<List<Assignment>>(HttpStatus.BAD_REQUEST);
     }
 
     public ResponseEntity<List<Assignment>> getAllUserAssignments(@ApiParam(value = "",required=true) @PathVariable("asurite") String asurite) {
         String accept = request.getHeader("Accept");
         if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<Assignment>>(objectMapper.readValue("[ {  \"assignedAsurite\" : \"smurra11\",  \"evalType\" : \"p2p\",  \"asurite\" : \"jjbowma2\",  \"semester\" : \"Fall 2019\",  \"assignmentId\" : 54,  \"isComplete\" : false}, {  \"assignedAsurite\" : \"smurra11\",  \"evalType\" : \"p2p\",  \"asurite\" : \"jjbowma2\",  \"semester\" : \"Fall 2019\",  \"assignmentId\" : 54,  \"isComplete\" : false} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Assignment>>(HttpStatus.INTERNAL_SERVER_ERROR);
+            List<Assignment> assignmentList = new ArrayList<Assignment>();
+            Iterator<Assignment> accountIterator = assignmentRepository.findAll().iterator();
+            while(accountIterator.hasNext())
+            {
+                Assignment assignment = accountIterator.next();
+                if(assignment.getAsurite().equals(asurite)) {
+                    assignmentList.add(assignment);
+                }
             }
+            return new ResponseEntity<List<Assignment>>(assignmentList, HttpStatus.OK);
         }
 
-        return new ResponseEntity<List<Assignment>>(HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<List<Assignment>>(HttpStatus.BAD_REQUEST);
     }
 
-    public ResponseEntity<List<Assignment>> getActiveUserAssignments(String asurite) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<List<Assignment>>(objectMapper.readValue("[ {  \"assignedAsurite\" : \"smurra11\",  \"evalType\" : \"p2p\",  \"asurite\" : \"jjbowma2\",  \"semester\" : \"Fall 2019\",  \"assignmentId\" : 54,  \"isComplete\" : false}, {  \"assignedAsurite\" : \"smurra11\",  \"evalType\" : \"p2p\",  \"asurite\" : \"jjbowma2\",  \"semester\" : \"Fall 2019\",  \"assignmentId\" : 54,  \"isComplete\" : false} ]", List.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<List<Assignment>>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<List<Assignment>>(HttpStatus.NOT_IMPLEMENTED);
+    @Override
+    public ResponseEntity<List<Assignment>> getActiveUserAssignments(@ApiParam(value = "",required=true) @PathVariable("asurite") String asurite) {
+        return null;
     }
 
     public ResponseEntity<Void> updateAssignment(@ApiParam(value = "" ,required=true )  @Valid @RequestBody Assignment body) {
         String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
+        if (accept != null && accept.contains("application/json")) {
+            if (assignmentRepository.findOne(body.getAssignmentId()) == null) {
+                return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
+            }
+            else {
+                assignmentRepository.save(body);
+                return new ResponseEntity<Void>(HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
     }
 
 }
