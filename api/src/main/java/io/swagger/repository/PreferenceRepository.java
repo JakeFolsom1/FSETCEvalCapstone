869b40829c7ed0269964c47335ff6af32889e04c
Jedde Bowman <jjbowma2@gmail.com>
2019-12-04 00:02:42 -0700

    Added functions needed for auto-assign

--- api/src/main/java/io/swagger/repository/PreferenceRepository.java
+++ api/src/main/java/io/swagger/repository/PreferenceRepository.java
@@ -3,6 +3,9 @@ package io.swagger.repository;
 import io.swagger.model.Preference;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PreferenceRepository extends CrudRepository<Preference, Long> {
     Preference findByAsuriteAndPreferenceNumberAndSemester(String asurite, Long preferenceNumber, String semester);
+    List<Preference> findAllByAsuriteAndSemesterOrderByPreferenceNumber(String asurite, String semester);
 }
