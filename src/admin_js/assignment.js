19eebc2978b69d9edd77051239cefeaa27ee80fc
Jedde Bowman <jjbowma2@gmail.com>
2020-02-10 16:07:13 -0700

    Changed UI on assignment and assignment progress

--- src/admin_js/assignment.js
+++ src/admin_js/assignment.js
@@ -177,7 +177,7 @@ $(document).ready(() => {
     // sort or ensure data will be sorted by preference number
     // Object.keys(prefMap).forEach((val) => { prefMap[val] = prefMap[val].sort((a, b) => a.preferenceNumber > b.preferenceNumber); });
 
-    // you also need to figure out if they are a lead or not by calling the account api
+    // you also need to figure out if they are a lead or not by calling the account api or if team member is redone
     const tableData = Object.keys(prefMap).map(val => [val, false, prefMap[val][0].preferredAsurite, prefMap[val][1].preferredAsurite, prefMap[val][2].preferredAsurite]).concat(leadMap);
 
     $('#assignmentTable').DataTable({
@@ -187,17 +187,7 @@ $(document).ready(() => {
         data: tableData,
         columns: [
             {
-                title: "Tutor Name",
-                render: (data, _type, _row) => {
-                    return `<a
-                                id="${data}AssignAnchor"
-                                data-toggle="collapse"
-                                data-parent="#assignmentTable"
-                                href="#${data}AssignCollapse"
-                                style="text-decoration: none; color: black;"
-                            >${data}  <span>&#x1F6C8;</span>
-                            </a>`;
-                }
+                title: "Tutor Name"
             },
             {
                 title: "Lead",
@@ -241,84 +231,113 @@ $(document).ready(() => {
             {
                 title: "Actions",
                 render: (_data, _type, row) => {
-                    return `<button id="${row[0]}AutoAssignButton" class="btn btn-primary" onclick="autoAssign('${row[0]}')" ${row[1] ? "disabled" : ""}>Auto-Assign</button>`;
+                    const tutorName = row[0];
+                    const lead = leadTeams.find(value => tutorName === value.leadAsurite);
+                    const tutorsLead = leadTeams.find(value => value.teamMembers.includes(tutorName));
+                    return (`
+                        <button id="${tutorName}AssignButton" class="btn btn-primary" data-toggle="modal" data-target="#${tutorName}AssignModal">Assign</button>
+                        <button id="${tutorName}AutoAssignButton" class="btn btn-primary" onclick="autoAssign('${tutorName}')" ${row[1] ? "disabled" : ""}>Auto-Assign</button>
+                        <div class="modal fade" id="${tutorName}AssignModal" tabindex="-1">
+                            <div class="modal-dialog modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h3 class="modal-title" id="addSemesterModalLabel">
+                                            Assign ${tutorName}
+                                        </h3>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form id="${tutorName}AssignForm">
+                                            ${tutorList.includes(tutorName) ? `
+                                                <div class="form-group">
+                                                    <label for="${tutorName}Lead">Lead Evaluation:</label>
+                                                    <select id="${tutorName}Lead" class="form-control" readonly>
+                                                        <option>${tutorsLead.leadAsurite}</option>
+                                                    </select>
+                                                </div>` +
+                            Array.from(Array(numAssignments).keys()).map((_val, index) => {
+                                const existingAssignment = assignments.find(value => value.asurite === tutorName && value.evalNumber === index + 1);
+                                return (`
+                                    <div class="form-group">
+                                        <label for="${tutorName}Peer${index}">Peer Evaluation ${index + 1}:</label>
+                                        <select id="${tutorName}Peer${index}" class="form-control">
+                                            <option>None</option>
+                                            ${tutorList.filter(value => value !== tutorName).map(value =>
+                                    `<option ${existingAssignment && existingAssignment.assignedAsurite === value ? "selected" : ""}>${value}</option>`)}
+                                        </select>
+                                    </div>
+                                `)
+                            }).join("") :
+                            lead ? lead.teamMembers.map((value, index) => {
+                                return (`
+                                    <div class="form-group">
+                                        <label for="${tutorName}Team${index}">Team Member Evaluation ${index + 1}:</label>
+                                        <select id="${tutorName}Team${index}" class="form-control" readonly>
+                                            <option>${value}</option>
+                                        </select>
+                                    </div>
+                                `)
+                            }).join("") :
+                                '<h2>Error</h2>'
+                        }
+                                        </form >
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button
+                                            type="button"
+                                            class="btn btn-outline-primary"
+                                            data-dismiss="modal"
+                                        >
+                                            Close
+                                        </button>
+                                        <button
+                                            type="submit"
+                                            id="${tutorName}SaveAssignButton"
+                                            form="${tutorName}AssignForm"
+                                            class="btn btn-primary"
+                                            ${lead ? "disabled" : ""}
+                                        >
+                                            Save
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `);
                 }
             }
         ]
     });
-    // the styling is slightly strange here consider redesigning this UI
-    const addCollapse = () => {
-        tableData.forEach(val => {
-            const lead = leadTeams.find(value => val[0] === value.leadAsurite);
-            const tutorsLead = leadTeams.find(value => value.teamMembers.includes(val[0]));
-            const form = `<form id="${val[0]}AssignForm">
-            ${tutorList.includes(val[0]) ?
-                    `<div class="form-group">
-                    <label for="${val[0]}Lead">Lead Evaluation:</label>
-                    <select id="${val[0]}Lead" class="form-control" readonly>
-                        <option>${tutorsLead.leadAsurite}</option>
-                    </select>
-                </div>` +
-                    Array.from(Array(numAssignments).keys()).map((_val, index) => {
-                        const existingAssignment = assignments.find(value => value.asurite === val[0] && value.evalNumber === index + 1);
-                        return `<div class="form-group">
-                            <label for="${val[0]}Peer${index}">Peer Evaluation ${index + 1}:</label>
-                            <select id="${val[0]}Peer${index}" class="form-control">
-                                <option>None</option>
-                                ${tutorList.filter(value => value !== val[0]).map(value =>
-                            `<option ${existingAssignment && existingAssignment.assignedAsurite === value ? "selected" : ""}>${value}</option>`)}
-                            </select>
-                        </div>`
-                    }).join("") :
-                    lead ? lead.teamMembers.map((value, index) => {
-                        return `<div class="form-group">
-                <label for="${val[0]}Team${index}">Team Member Evaluation ${index + 1}:</label>
-                <select id="${val[0]}Team${index}" class="form-control" readonly>
-                        <option>${value}</option>
-                </select>
-            </div>`
-                    }).join("") : '<h2>Error</h2>'
-                }
-            <button id="${val[0]}AssignButton" class="btn btn-primary" type="submit" ${lead ? "disabled" : ""}>Save Assignment</button>
-            </form > `;
 
-            const parentRow = document.getElementById(val[0] + 'AssignAnchor').parentElement.parentElement;
-            parentRow.insertAdjacentHTML('afterend', `<tr id = "${val[0]}AssignCollapse" class="panel-collapse collapse" > <td class="panel-body" style="width: 17%">${form}</td> <td /> <td /><td /><td /><td /></<tr>`)
-        });
+    tableData.forEach(row => {
+        const tutorName = row[0];
+        document.getElementById(`${tutorName}AssignForm`).addEventListener('submit', event => {
+            // lead form cannot currently be submitted as button is disabled
+            // if this changes, a lead submission needs to be considered
+            event.preventDefault();
+            const values = Array.from(Array(numAssignments).keys()).map(value => document.getElementById(`${tutorName}Peer${value}`).value);
+            const button = document.getElementById(`${tutorName}SaveAssignButton`);
+            let isValid = new Set(values).size === values.length;
+            if (isValid) {
+                button.setAttribute("disabled", "true");
+                button.innerHTML = "Saved Assignments";
+                setTimeout(() => {
+                    button.removeAttribute("disabled");
+                    button.innerHTML = "Save Assignments";
+                }, 1000);
+                console.log("Saving assignments as: " + values);
+            }
+            else {
+                button.setAttribute("disabled", "true");
+                button.innerHTML = "Error: Duplicates found";
+                setTimeout(() => {
+                    button.removeAttribute("disabled");
+                    button.innerHTML = "Save Assignments";
+                }, 1000);
+                console.log("Duplicates found: " + values);
+            }
 
-        tableData.forEach(val => {
-            document.getElementById(`${val[0]}AssignForm`).addEventListener('submit', event => {
-                // lead form cannot currently be submitted as button is disabled
-                // if this changes, a lead submission needs to be considered
-                event.preventDefault();
-                const values = Array.from(Array(numAssignments).keys()).map(value => document.getElementById(`${val[0]}Peer${value}`).value);
-                const button = document.getElementById(`${val[0]}AssignButton`);
-                let isValid = new Set(values).size === values.length;
-                if (isValid) {
-                    button.setAttribute("disabled", "true");
-                    button.innerHTML = "Saved Assignments";
-                    setTimeout(() => {
-                        button.removeAttribute("disabled");
-                        button.innerHTML = "Save Assignments";
-                    }, 1000);
-                    console.log("Saving assignments as: " + values);
-                }
-                else {
-                    button.setAttribute("disabled", "true");
-                    button.innerHTML = "Error: Duplicates found";
-                    setTimeout(() => {
-                        button.removeAttribute("disabled");
-                        button.innerHTML = "Save Assignments";
-                    }, 1000);
-                    console.log("Duplicates found: " + values);
-                }
-
-            })
         })
-    };
-    addCollapse();
-    // have to add the collapse targets again after a re-order destroys them
-    $('#assignmentTable').on('order.dt', addCollapse);
+    })
 });
 
 const autoAssign = asurite => {
