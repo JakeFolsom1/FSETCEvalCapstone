ff4e51d7a1a6ccecdf71d2f75b3eab0457dbfbeb
Jedde Bowman <jjbowma2@gmail.com>
2020-02-18 18:37:41 -0700

    Allowed any origin to access api during dev

--- api/src/main/java/io/swagger/Swagger2SpringBoot.java
+++ api/src/main/java/io/swagger/Swagger2SpringBoot.java
@@ -6,8 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -29,6 +33,16 @@ public class Swagger2SpringBoot extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Swagger2SpringBoot.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*"); // change to client host after launch
+            }
+        };
     }
 
 }
