4ad12a0dcb91ecbc69bee9bb110d9f0b16c17c06
Cole Stanfield <cjstanfi@asu.edu>
2020-03-05 21:14:22 -0700

    Added dynamic table for completed evals (admin view)

--- src/admin_js/completedEval.js
+++ src/admin_js/completedEval.js
@@ -1,46 +1,82 @@
-$(document).ready(() => {
-    $.getJSON(apiUrl + "/completedEvaluations",
-        function (completedEvalJson) {
-            const completedEvals = completedEvalJson.map(completedEval => [
-                "Spring 2019"/*replace when API is updated*/,
-                completedEval.evaluator + (completedEval.evalType === "l2t" ? " - Lead" : ""),
-                completedEval.evaluatee + (completedEval.evalType === "t2l" ? " - Lead" : "")
-            ]);
+const dummyData = [
+    {
+        "semester": "Fall 2019",
+        "evaluator": "aarunku3",
+        "evaluatee": "amevawal",
+        "evalType": "l2t"
+    },
+    {
+        "semester": "Fall 2019",
+        "evaluator": "dvaldera",
+        "evaluatee": "eclark18",
+        "evalType": "t2l"
+    },
+    {
+        "semester": "Fall 2019",
+        "evaluator": "aarunku3",
+        "evaluatee": "amevawal",
+        "evalType": "l2t"
+    },
+    {
+        "semester": "Fall 2019",
+        "evaluator": "aarunku3",
+        "evaluatee": "amevawal",
+        "evalType": "l2t"
+    },
+];
 
+$(document).ready(() => {
+    let completedEvals = [];
+    let names = [];
+    $.when(
+        $.getJSON(apiUrl + "/completedEvaluations",
+            function (completedEvalJson) {
+                console.log(completedEvalJson);
+                completedEvals = completedEvalJson;
+            }
+        ),
+        $.getJSON(apiUrl + "/accounts/names",
+            function (namesJson) {
+                console.log(namesJson);
+                names = namesJson;
+            }
+        )
+    ).then(function () {
+        const tableData = dummyData.map(eval => [
+            eval.semester,
+            names[eval.evaluator] + (eval.evalType === "l2t" ? " - Lead" : ""),
+            names[eval.evaluatee] + (eval.evalType === "t2l" ? " - Lead" : "")
+        ])
             $('#completedEvaluationTable').DataTable({
                 stripe: true,
                 paging: false,
                 searching: false,
                 info: false,
-                data: completedEvals,
+                data: tableData,
                 columns: [
-                    {
-                        title: "Semester"
-                    },
-                    {
-                        title: "Evaluator Name"
-                    },
-                    {
-                        title: "Evaluatee Name"
-                    },
+                    { title: "Semester" },
+                    { title: "Evaluator Name" },
+                    { title: "Evaluatee Name" },
                     {
                         title: "Actions",
-                        render: (data, _type) => {
+                        render: () => {
                             //Use data variable to pass the evaluation parameters to the evaluations page.
                             let viewButton =
-                                `<button 
-                                class="btn btn-secondary" 
-                                style="border-color: #8C1D40;"
-                                onclick="location.href = './evaluations.html';" id="myButton">
-                                View
-                                </button>`;
+                                `<button
+                            class="btn btn-primary"
+                            style="border-color: #8C1D40;"
+                            onclick="location.href = './evaluations.html';" id="myButton">
+                            View
+                            </button>`;
                             return viewButton;
                         }
                     }
                 ]
-
-            });
+            })
         }
-    );
-
+    )
 });
+
+const searchTutor = (name) => {
+    
+}
