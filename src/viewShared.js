17fe465ed57104ad3eb159869b0cbd24a16a307e
Jedde Bowman <jjbowma2@gmail.com>
2020-03-11 09:20:02 -0700

    Fixing tables in lead view

--- src/viewShared.js
+++ src/viewShared.js
@@ -1,78 +1,66 @@
-const dummyData = [
-    {
-        "semester": "Fall 2019",
-        "evaluator": "aarunku3",
-        "evalType": "l2t"
-    },
-    {
-        "semester": "Fall 2019",
-        "evaluator": "dvaldera",
-        "evalType": "t2l"
-    },
-    {
-        "semester": "Fall 2019",
-        "evaluator": "aarunku3",
-        "evalType": "l2t"
-    },
-    {
-        "semester": "Fall 2019",
-        "evaluator": "aarunku3",
-        "evalType": "l2t"
-    },
-];
+// const sharedEvals = [
+//     {
+//         "evalType": "t2l",
+//         "evaluatee": "smurra11",
+//         "evaluator": "jjbowma2",
+//         "isShared": true,
+//         "questionsAndResponses": [
+//             {
+//                 "question": {
+//                     "questionNumber": 3,
+//                     "questionPrompt": "Does tutor obey all procedures and policies of the center?",
+//                     "questionType": "freeResponse"
+//                 },
+//                 "response": "John does not wear his sash when he's on shift."
+//             }
+//         ],
+//         "semester": "Fall 2019"
+//     }
+// ]
 
+let sharedEvals = [], names = {}, leadAsurite = "aarunku3";
 $(document).ready(() => {
-    let completedEvals = [];
-    let names = [];
     $.when(
-        $.getJSON(apiUrl + "/completedEvaluations",
+        $.getJSON(apiUrl + `/completedEvaluations/shared/${leadAsurite}`,
             function (completedEvalJson) {
-                console.log(completedEvalJson);
-                completedEvals = completedEvalJson;
+                sharedEvals = completedEvalJson;
             }
         ),
         $.getJSON(apiUrl + "/accounts/names",
             function (namesJson) {
-                console.log(namesJson);
                 names = namesJson;
             }
         )
     ).then(function () {
-        const tableData = dummyData.map(eval => [
+        const tableData = sharedEvals.map(eval => [
             eval.semester,
             names[eval.evaluator],
-	    (eval.evalType === "l2t" ? " - Lead" : "")
+            null
         ])
-            $('#completedEvaluationTable').DataTable({
-                stripe: true,
-                paging: false,
-                searching: false,
-                info: false,
-                data: tableData,
-                columns: [
-                    { title: "Semester" },
-                    { title: "Evaluator Name" },
-                    { title: "Lead",
-                      render: data => {
-                        	return data ? '<div class="dot" />' : "";
-                      }
-                    },
-                    {
-                        title: "Actions",
-                        render: () => {
-                            //Use data variable to pass the evaluation parameters to the evaluations page.
-                            let viewButton =
-                                `<button
+        $('#completedEvaluationTable').DataTable({
+            stripe: true,
+            paging: false,
+            searching: false,
+            info: false,
+            data: tableData,
+            columns: [
+                { title: "Semester" },
+                { title: "Evaluator Name" },
+                {
+                    title: "Actions",
+                    render: () => {
+                        //Use data variable to pass the evaluation parameters to the evaluations page.
+                        let viewButton =
+                            `<button
                             class="btn btn-primary"
                             style="border-color: #8C1D40;"
                             onclick="location.href = './evaluations.html';" id="myButton">
                             View
                             </button>`;
-                            return viewButton;
-                        }
+                        return viewButton;
                     }
-                ]
-            })
-        }
-    )
+                }
+            ]
+        })
+    })
 });
