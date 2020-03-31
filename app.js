4d74ab3cd8c015cd5c3792d050cde4ce8c6e9616
Jedde Bowman <jjbowma2@gmail.com>
2020-03-30 23:14:06 -0700

    Added express-auth-cas and express-session libs

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const path = require('path');
 const fetch = require('node-fetch');
+const session = require('express-session');
+const CASAuthentication = require('cas-authentication');
 const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(session({
+    secret: 'super secret key',
+    resave: false,
+    saveUninitialized: true
+}));
+
+const cas = new CASAuthentication({
+    cas_url: 'https://sod819.fulton.asu.edu/cas',
+    service_url: '/',
+    cas_version: '3.0',
+    renew: false,
+    is_dev_mode: false,
+    dev_mode_user: '',
+    dev_mode_info: {},
+    session_name: 'cas_user',
+    session_info: 'cas_userinfo',
+    destroy_session: false,
+    single_logout: false
+});
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
-app.get('/', function (req, res) {
+app.get('/', cas.bounce, function (req, res) {
 
     // get asurite from CAS
-    // admin
-    const asurite = 'caphill7';
-
-    // lead
-    // const asurite = 'aarunku3';
-
-    // tutor
-    // const asurite = 'aguha4';
-
+    const asurite = req.session[cas.session_name];
 
     fetch(`https://sod819.fulton.asu.edu/eval-app/staff/${asurite}`, {
         headers: { 'Accept': 'application/json' }
@@ -41,5 +55,6 @@ app.get('/', function (req, res) {
         });
 
 });
+
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
