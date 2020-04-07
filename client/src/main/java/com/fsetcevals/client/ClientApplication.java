05a9f4c5e8c25d083c96f68b310064c0f43cab7b
Jedde Bowman <jjbowma2@gmail.com>
2020-04-06 21:54:42 -0700

    Resolved path for static resources

--- client/src/main/java/com/fsetcevals/client/ClientApplication.java
+++ client/src/main/java/com/fsetcevals/client/ClientApplication.java
@@ -2,12 +2,19 @@ package com.fsetcevals.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 // Created following a guide posted by baeldung
 // https://www.baeldung.com/spring-security-cas-sso
 
 @SpringBootApplication
-public class ClientApplication {
+public class ClientApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ClientApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
     }
