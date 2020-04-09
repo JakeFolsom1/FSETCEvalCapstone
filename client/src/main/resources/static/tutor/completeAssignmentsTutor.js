b643e6ee49a215684370336b9461021ebc3a533a
Cole Stanfield (Student) <cjstanfi@sundevils.asu.edu>
2020-04-08 19:56:52 -0700

    Fixed admin view modal. Completed tutor assignments

--- client/src/main/resources/static/tutor/completeAssignmentsTutor.js
+++ client/src/main/resources/static/tutor/completeAssignmentsTutor.js
@@ -1,23 +1,16 @@
 let userAssignments = [];
 let names = {};
-let completedEvals = [];
+
 $(document).ready(() => {
     reloadAssignments();
 });
 
 const reloadAssignments = () => {
-    let teamMembers = [];
     $.when(
         $.getJSON(apiUrl + `/assignments/active/${asurite}`, function (
             assignmentsJson
         ) {
             userAssignments = assignmentsJson;
-        }),
-        $.getJSON(apiUrl + `/teamMembers/${asurite}`, function (teamMemberJson) {
-            teamMembers = teamMemberJson;
-        }),
-        $.getJSON(apiUrl + "/completedEvaluations", function (completedEvalJson) {
-            completedEvals = completedEvalJson;
         }),
         $.getJSON(apiUrl + "/staff/names", function (namesJson) {
             names = namesJson;
@@ -57,55 +50,6 @@ const reloadAssignments = () => {
                 },
             ],
         });
-
-        completedEvals = completedEvals.filter((eval) =>
-            teamMembers.includes(
-                Object.keys(names).find((key) => names[key] == eval.evaluatee)
-            )
-        );
-        const completedEvalsTableData = completedEvals.map((eval) => [
-            eval.semester,
-            eval.evaluator,
-            eval.evaluatee,
-            null,
-        ]);
-        let completedEvalTable = $("#completedEvaluationTable").DataTable({
-            destroy: true,
-            stripe: true,
-            paging: false,
-            searching: true,
-            info: false,
-            data: completedEvalsTableData,
-            columns: [
-                {
-                    title: "Semester",
-                    render: data => {
-                        const semester = (data.includes("fall") ? "Fall" : data.includes("spring") ? "Spring" : data.includes("summer") ? "Summer" : "Invalid Semester");
-                        const year = data.substr(-2);
-                        return `${semester} 20${year}`;
-                    }
-                },
-                { title: "Evaluator" },
-                { title: "Evaluatee" },
-                {
-                    title: "Actions",
-                    render: (data, _type, row) => {
-                        //Use data variable to pass the evaluation parameters to the evaluations page.
-                        let viewButton = `<button
-                        class="btn btn-primary"
-                        style="border-color: #8C1D40;"
-                        onclick="viewEvaluation('${names[asurite]}', '${row[2]}')" id="myButton">
-                        View
-                        </button>`;
-                        return viewButton;
-                    },
-                },
-            ],
-        });
-
-        $("#searchText").on("keyup change", function () {
-            completedEvalTable.search(this.value).draw();
-        });
     });
 }
 
@@ -122,7 +66,7 @@ const buildEvaluation = (evaluatee) => {
     $("#evalHeader h3").remove();
     $("#submitEvalButton").show();
     $.when(
-        $.getJSON(apiUrl + "/questions/l2t", function (result) {
+        $.getJSON(apiUrl + `/questions/${assignment.evalType}`, function (result) {
             evalQuestions = result;
         })
     ).then(function () {
@@ -207,53 +151,3 @@ const buildEvaluation = (evaluatee) => {
     });
 };
 
-const viewEvaluation = (evaluator, evaluatee) => {
-    //Clear any old evaluations in the modal. There is probably a better way to do this
-    $("#questionsAndResponses").empty();
-    $("#evalHeader h3").remove();
-    $("#submitEvalButton").hide();
-
-    //search for the evaluation and questions
-    const currentEval = completedEvals.find(
-        (evaluation) =>
-            evaluator == evaluation.evaluator && evaluatee == evaluation.evaluatee
-    );
-    const questions = currentEval.questionsAndResponses;
-
-    //Add the title to the Modal
-    const title = `<h3>${evaluator}'s Evaluation of ${evaluatee} </h3>`;
-    $("#evalHeader").append(title);
-
-    const sharedRadioInput = `<div>
-            <p>Is this evaluation shared with ${evaluatee}?</p>
-            ${
-        currentEval.isShared == true
-            ? `<input type="radio" id="isSharedYes" checked="checked" disabled="true"><label for="isSharedYes">Yes</label>
-            <input type="radio" id="isSharedNo" disabled="true"><label for="isSharedNo">No</label>`
-            : `<input type="radio" id="isSharedYes" disabled="true"><label for="isSharedYes">Yes</label>
-            <input type="radio" id="isSharedNo" checked="checked" disabled="true"><label for="isSharedNo">No</label>`
-    }
-         </div>`;
-
-    //Add the questions to the modal. Needs styling.
-    $.each(questions.sort((q1, q2) => q1.question.questionNumber - q2.question.questionNumber), (index, question) => {
-        const innerHTML = `<li>
-                <h4 style="font-weight: bold">Question ${question.question.questionNumber}:</h4>
-                <p class="tab-eval">  ${question.question.questionPrompt}</p>
-                ${
-            question.question.questionType == "numeric"
-                ? `<input type="range" min="1" max="5" id="response${index}" value="${question.response}" disabled="true"/><p class="text-center">${question.response}</p>`
-                : question.question.questionType == "yesNo"
-                ? `<input type="radio" id="response${index}" ${question.response == "true" ? "checked" : ""} disabled="true"/>
-                      <label for="response${index}">Yes</label>
-                      <input type="radio" id="response${index}No" ${question.response == "true" ? "" : "checked"} disabled="true"/>
-                      <label for="response${index}No">No</label>`
-                : `<textarea class="form-control" id="response${index}" disabled="true" >${question.response}</textarea>`
-        }
-                <br>
-             </li>`;
-        $("#questionsAndResponses").append(innerHTML);
-    });
-    $("#questionsAndResponses").append(sharedRadioInput);
-    $("#testmodal").modal("show");
-};
