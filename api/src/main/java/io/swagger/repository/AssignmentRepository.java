f5413c6d643ccffaa1c83817c6c617cb823546ab
Jedde Bowman <jjbowma2@gmail.com>
2020-04-09 16:01:04 -0700

    Fixed bug that skipped the first evaluation slot

--- api/src/main/java/io/swagger/repository/AssignmentRepository.java
+++ api/src/main/java/io/swagger/repository/AssignmentRepository.java
@@ -2,14 +2,13 @@ package io.swagger.repository;
 
 import io.swagger.model.Assignment;
 import io.swagger.model.Question;
-import io.swagger.model.Semester;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface AssignmentRepository extends CrudRepository<Assignment, Long> {
     Assignment findByAsuriteAndAssignmentNumberAndEvalTypeAndSemesterName(String asurite, Long assignmentNumber, Question.EvalType evalType, String semesterName);
-    List<Assignment> findAllByAssignedAsuriteAndSemesterName(String assignedAsurite, String semesterName);
+    List<Assignment> findAllByAssignedAsuriteAndSemesterNameAndEvalType(String assignedAsurite, String semesterName, Question.EvalType evalType);
     Assignment findByAsuriteAndAssignedAsurite(String asurite, String assignedAsurite);
     List<Assignment> findDistinctByIsCompleteAndSemesterName(boolean isComplete, String semesterName);
     List<Assignment> findAllByIsCompleteAndAsuriteAndSemesterName(boolean isComplete, String asurite, String semesterName);
@@ -17,7 +16,7 @@ public interface AssignmentRepository extends CrudRepository<Assignment, Long> {
     List<Assignment> findAllByIsCompleteAndAssignedAsurite(boolean isComplete, String assignedAsurite);
     List<Assignment> findAllByAssignmentId(Long assignmentId);
     List<Assignment> findAllByAsurite(String asurite);
-    List<Assignment> findAllByAsuriteAndSemesterName(String asurite, String semesterName);
+    List<Assignment> findAllByAsuriteAndSemesterNameAndEvalType(String asurite, String semesterName, Question.EvalType evalType);
     void deleteAllBySemesterName(String semesterName);
     void deleteAllBySemesterNameAndEvalType(String semesterName, Question.EvalType evalType);
     void deleteAssignmentByAssignedAsuriteAndAsuriteAndSemesterName(String assignedAsurite, String asurite, String semesterName);
